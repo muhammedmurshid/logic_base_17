8d8fde959ddee7f7bbe9e014dcdb3412a11121f2
muhammed murshid <muhsink00000@gmail.com>
2025-01-12 12:21:12 +0530

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,28 @@
+{
+    'name': 'Logic Base',
+    'version': '1.0.0',
+    'summary': 'logic Batches',
+    'description': """
+        A more detailed description of the module.
+    """,
+    'author': 'Murshid',
+    'website': 'https://www.yourwebsite.com',
+    'category': 'Specific Category',
+    'license': 'LGPL-3',
+    'depends': [
+        'base', 'hr','openeducat_core'  # List of module dependencies
+
+        # Add other module dependencies here
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',  # Access rights
+        'security/ir.model.access.csv',
+        'views/base.xml'
+
+    ],
+
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+
+}
