e2524a2c42af3927304570aaef62ad7fe9e54f95
muhammed murshid <muhsink00000@gmail.com>
2025-06-17 17:09:13 +0530

    user inheritance removed

--- models/user_inheritance.py
+++ models/user_inheritance.py
@@ -3,5 +3,5 @@ from odoo import fields,models,api,_
 class InheritUserDetails(models.Model):
     _inherit = 'res.users'
 
-    designation = fields.Char(related='employee_id.job_title', string="Designation")
-    department_id = fields.Char(related="employee_id.department_id.name", string="Department")
+    # designation = fields.Char(related='employee_id.job_title', string="Designation")
+    # department_id = fields.Char(related="employee_id.department_id.name", string="Department")
